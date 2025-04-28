5afea1c999dfef5610243f7b51d79844e45f8add
Paulo Vieira <paulovieiradev@outlook.com>
2025-04-28 00:13:27 -0300

    feat: Implemented logs for HubSpotOAuthController

--- src/main/java/com/github/paulovieirajr/meetime/rest/controller/oauth/HubSpotOAuthController.java
+++ src/main/java/com/github/paulovieirajr/meetime/rest/controller/oauth/HubSpotOAuthController.java
@@ -4,6 +4,8 @@ import com.github.paulovieirajr.meetime.rest.client.HubSpotOAuthClient;
 import com.github.paulovieirajr.meetime.rest.dto.AuthUrlResponse;
 import com.github.paulovieirajr.meetime.rest.dto.HubSpotTokenResponse;
 import com.github.paulovieirajr.meetime.token.OAuthSessionStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +21,12 @@ import java.util.UUID;
 
 import static com.github.paulovieirajr.meetime.rest.constants.HubSpotParams.*;
 import static com.github.paulovieirajr.meetime.rest.constants.HubSpotPathController.*;
+import static com.github.paulovieirajr.meetime.rest.controller.oauth.log.OAuthLogger.*;
 
 @RestController
 public class HubSpotOAuthController implements HubSpotOAuthControllerSwagger {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HubSpotOAuthController.class);
 
     @Value("${hubspot.client.id}")
     private String clientId;
@@ -51,17 +56,22 @@ public class HubSpotOAuthController implements HubSpotOAuthControllerSwagger {
 
     @GetMapping(OAUTH_URL_PATH)
     public ResponseEntity<AuthUrlResponse> getAuthorizationUrl() {
+        LOGGER.info(LOG_OAUTH_URL_ENDPOINT.getMessage());
+        LOGGER.info(LOG_GENERATE_OAUTH_URL.getMessage());
         AuthUrlResponse authUrlResponse = new AuthUrlResponse(UriComponentsBuilder
                 .fromUriString(urlForAuthorization)
                 .queryParam(CLIENT_ID.getValue(), clientId)
                 .queryParam(SCOPE.getValue(), scopes)
                 .queryParam(REDIRECT_URI.getValue(), redirectUri)
                 .toUriString());
+        LOGGER.info(LOG_SUCCESS_OAUTH_URL.getMessage());
         return ResponseEntity.ok(authUrlResponse);
     }
 
     @GetMapping(OAUTH_CALLBACK_PATH)
     public ResponseEntity<?> oauthCallback(@RequestParam String code) {
+        LOGGER.info(LOG_OAUTH_CALLBACK_ENDPOINT.getMessage(), code);
+        LOGGER.info(LOG_OAUTH_BUILDING_REQUEST.getMessage());
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add(GRANT_TYPE.getValue(), AUTHORIZATION_CODE.getValue());
         body.add(CLIENT_ID.getValue(), clientId);
@@ -69,10 +79,15 @@ public class HubSpotOAuthController implements HubSpotOAuthControllerSwagger {
         body.add(REDIRECT_URI.getValue(), redirectUri);
         body.add(CODE.getValue(), code);
 
+        LOGGER.info(LOG_OAUTH_HUBSPOT_TOKEN_ENDPOINT.getMessage());
         HubSpotTokenResponse hubSpotTokenResponse = hubSpotOAuthClient.exchangeToken(body);
+        LOGGER.info(LOG_OAUTH_HUBSPOT_TOKEN_SUCCESS.getMessage());
+
         String sessionId = UUID.randomUUID().toString();
+        LOGGER.info(LOG_NEW_SESSION_ID.getMessage(), sessionId);
         oAuthSessionStore.saveToken(sessionId, hubSpotTokenResponse);
 
+        LOGGER.info(LOG_REDIRECTING.getMessage(), sessionId);
         URI redirectUri = UriComponentsBuilder
                 .fromUriString(urlForOAuthSuccess)
                 .queryParam(SESSION_ID.getValue(), sessionId)
@@ -86,7 +101,9 @@ public class HubSpotOAuthController implements HubSpotOAuthControllerSwagger {
 
     @GetMapping(ACCESS_TOKEN_PATH)
     public ResponseEntity<?> getAccessToken(@RequestParam String sessionId) {
+        LOGGER.info(LOG_ACCESS_TOKEN_ENDPOINT.getMessage(), sessionId);
         HubSpotTokenResponse tokenResponse = oAuthSessionStore.getToken(sessionId);
+        LOGGER.info(LOG_TOKEN_RECOVERED_SUCCESS.getMessage());
         return ResponseEntity.ok(tokenResponse);
     }
 }
