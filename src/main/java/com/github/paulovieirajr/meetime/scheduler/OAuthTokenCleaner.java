7b8216cad2d3e4a7ff4715139eddfb748670ff23
Paulo Vieira <paulovieiradev@outlook.com>
2025-04-28 00:26:48 -0300

    feat: Implemented logs for OAuthTokenCleaner

--- src/main/java/com/github/paulovieirajr/meetime/scheduler/OAuthTokenCleaner.java
+++ src/main/java/com/github/paulovieirajr/meetime/scheduler/OAuthTokenCleaner.java
@@ -1,11 +1,17 @@
 package com.github.paulovieirajr.meetime.scheduler;
 
 import com.github.paulovieirajr.meetime.token.OAuthSessionStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import static com.github.paulovieirajr.meetime.scheduler.log.SchedulerLogger.LOG_REMOVING_EXPIRED_TOKENS;
+
 @Component
 public class OAuthTokenCleaner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OAuthTokenCleaner.class);
 
     private final OAuthSessionStore oAuthSessionStore;
 
@@ -15,6 +21,7 @@ public class OAuthTokenCleaner {
 
     @Scheduled(cron = "${hubspot.scheduler.cron}")
     public void cleanExpiredTokens() {
+        LOGGER.info(LOG_REMOVING_EXPIRED_TOKENS.getMessage());
         oAuthSessionStore.cleanExpiredTokens();
     }
 }
---
+++ src/main/java/com/github/paulovieirajr/meetime/scheduler/log/SchedulerLogger.java
@@ -0,0 +1,15 @@
+package com.github.paulovieirajr.meetime.scheduler.log;
+
+public enum SchedulerLogger {
+    LOG_REMOVING_EXPIRED_TOKENS("Tarefa agendada para limpar tokens expirados do HubSpot iniciada");
+
+    private final String message;
+
+    SchedulerLogger(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
